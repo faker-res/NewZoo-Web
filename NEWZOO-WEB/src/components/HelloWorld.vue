335a7f60dcd34700c7a2652e50c4c4a8809a0bd4
Anthony <y950727@gmail.com>
2020-01-08 17:26:10 +0800

    1

--- NEWZOO-WEB/src/components/HelloWorld.vue
+++ NEWZOO-WEB/src/components/HelloWorld.vue
@@ -1,32 +1,57 @@
 <template>
-   <el-button type="primary"></el-button>
+  <div>
+    <el-form ref="form" :model="form" label-width="80px">
+
+      <el-form-item label="用户名" label-width="100px">
+        <el-input v-model="form.username"></el-input>
+      </el-form-item>
+
+      <el-form-item label="密码" inline="true" :disable="true">
+        <el-input v-model="form.password"></el-input>
+      </el-form-item>
+
+      <el-form-item label="记住我">
+        <el-switch v-model="form.rememberMe"></el-switch>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary" @click="login">登录</el-button>
+        <el-button type="primary" @click="register">注册</el-button>
+      </el-form-item>
+
+    </el-form>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        form: {
+          username: '',
+          password: '',
+          rememberMe: ''
+        }
+      }
+    },
+    methods: {
+      login(){
+        alert("登录");
+      },
+      register(){
+        alert("注册");
+      }
     }
   }
-}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
+  el-form {
+    margin: auto;
+    height: 100px;
+    width: 100px;
+  }
+
 </style>
