abb8f0e24fe804a2f56ce7587ea4117bad61cda2
kayden <kayden@lzkj.com>
2020-01-08 18:00:31 +0800

    更改样式

--- NEWZOO-WEB/src/components/HelloWorld.vue
+++ NEWZOO-WEB/src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div>
     <el-form ref="form" :model="form" label-width="80px">
 
-      <el-form-item label="用户名" label-width="100px">
+      <el-form-item label="用户名" >
         <el-input v-model="form.username"></el-input>
       </el-form-item>
 
@@ -46,12 +46,12 @@
   }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 
-  el-form {
+  .el-form {
     margin: auto;
     height: 100px;
-    width: 100px;
+    width: 400px;
+    
   }
-
 </style>
